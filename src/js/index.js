b4f149bd0689b71fb84027d7658480c464535212
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-22 11:16:12 -0700

    update .gitignore and index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/styles.css';
+import {Template} from './BUSINESS.js';
+
 //UI Logic
 
 //JQUERY UI Logic
